8ac6ce8cbea9cc4c4f6550643ddf78514e753ae6
Станислав Маяцкий <stasm226@gmail.com>
2025-04-11 18:17:51 +0500

    chore: make migrations for habit and user models

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,10 @@ URL configuration for config project.
 """
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("habit/", include("habit.urls", namespace="habit")),
-    path("user/", include("user.urls", namespace="user")),
+    # path("habit/", include("habit.urls", namespace="habit")),
+    # path("user/", include("user.urls", namespace="user")),
 ]
